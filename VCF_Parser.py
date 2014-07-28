00f293abddd1cb48bd780ff560f26563b14097bd
393099545@qq.com <cmiao@peach.(none)>
2014-07-29 01:18:04 +0800

    vcf class

---
+++ VCF_Parser.py
@@ -0,0 +1,60 @@
+#!/usr/lib/python
+from optparse import OptionParser
+
+msg_usage = 'just import these classes'
+descr ='''Define four classes. The first one GeneralVcf if used for any vcf
+fils, you can get chr, pos, Rbase, Abase, genotype info because these items
+in different vcf files at the same positons.Caveat: the raw vcf from samtools
+contain some lines which dont contain genotype info, so you should filter those
+lines firstly, then use this class.The second one is special for vcf generated
+by freebayse.Caveat: in freebayes vcf files, DP sometimes is not eqal with
+AO+RO....  The third one is only for GATK vcf files and the last class is
+used for samtools vcf files.Design these three classes respectively because
+DP, Acount, Rcount are in different positions in different vcf files generated
+by different SNP callers.
+'''
+optparser = OptionParser(usage = msg_usage, description = descr)
+
+options, args = optparser.parse_args()
+
+class GeneralVcf:
+    def __init__(self, line):
+        self.chr = line.split()[0]
+        self.pos = int(line.split()[1])
+        self.Rbase = line.split()[3]
+        self.Abase = line.split()[4]
+        self.genotype = line.split()[-1].split(':')[0]
+
+class FbVcf(GeneralVcf):
+    def __init__(self, line):
+        GeneralVcf.__init__(self, line)
+        self.DP = int(line.split()[-1].split(':')[1])
+        self.Rcount = int(line.split()[-1].split(':')[2])
+        self.Acount = int(line.split()[-1].split(':')[4])
+
+class GATKVcf(GeneralVcf):
+    def __init__(self, line):
+        GeneralVcf.__init__(self, line)
+        self.DP = int(line.split()[-1].split(':')[2])
+        self.Rcount = int(line.split()[-1].split(':')[1].split(',')[0])
+        self.Acount = int(line.split()[-1].split(':')[1].split(',')[1])
+
+class SBVcf(GeneralVcf):
+    def __init__(self, line):
+        GeneralVcf.__init__(self, line)
+        self.info_dict = {}
+        self.items = line.split()[7].split(';')
+        for k in self.items:
+            self.info_dict[k.split('=')[0]] = k.split('=')[1]
+        if 'DP4' in self.info_dict:
+            self.Rcount = int(self.info_dict['DP4'].split(',')[0]) + \
+int(self.info_dict['DP4'].split(',')[1])
+            self.Acount = int(self.info_dict['DP4'].split(',')[2]) + \
+int(self.info_dict['DP4'].split(',')[3])
+            self.DP = self.Rcount + self.Acount
+        else:
+            self.Rcount = 'noRecord'
+            self.Acount = 'noRecord'
+            self.DP = 'noRecord'
+
+
